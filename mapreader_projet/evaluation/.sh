fc36e7e5e169bc961a0c33568bd1bb1b4e65bcf9
Emiliegd14 <153830067+Emiliegd14@users.noreply.github.com>
2025-06-30 13:57:12 +0200

    Update .sh
    maj sorties résultats

--- mapreader_projet/evaluation/.sh
+++ mapreader_projet/evaluation/.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-# Active conda
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate maptext-eval
 
-# Évaluation DET data-sample
-python eval.py --gt data_eval/metadatagoodsample.json --pred data_eval/resultatsample.json --task "det"
+echo "Résultats Évaluation DET data-sample :" > resultats.txt
+python eval.py --gt data_eval/metadatagoodsample.json --pred data_eval/resultatsample.json --task det >> resultats.txt
 
-# Évaluation DETREC data-sample
-python eval.py --gt data_eval/metadatagoodsample.json --pred data_eval/resultatsample.json --task "detrec"
+echo -e "\nRésultats Évaluation DETREC data-sample :" >> resultats.txt
+python eval.py --gt data_eval/metadatagoodsample.json --pred data_eval/resultatsample.json --task detrec >> resultats.txt
 
-# Évaluation DET data-sample
-python eval.py --gt data_eval/metadatagood500.json --pred data_eval/resultat500.json --task "det"
+echo -e "\nRésultats Évaluation DET 500 samples :" >> resultats.txt
+python eval.py --gt data_eval/metadatagood500.json --pred data_eval/resultat500.json --task det >> resultats.txt
 
-# Évaluation DETREC data-sample
-python eval.py --gt data_eval/metadatagood500.json --pred data_eval/resultat500.json --task "detrec"
+echo -e "\nRésultats Évaluation DETREC 500 samples :" >> resultats.txt
+python eval.py --gt data_eval/metadatagood500.json --pred data_eval/resultat500.json --task detrec >> resultats.txt
